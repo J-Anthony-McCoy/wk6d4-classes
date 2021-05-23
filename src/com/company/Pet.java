fe8b511466fb553c8b35fa69a1f74e898d3eed1b
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-05-23 16:16:47 -0500

    Pets are done

---
+++ src/com/company/Pet.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Pet {
+//    Initialized all variables
+    private String name = "Sophie";
+    private int age = 6;
+    private String location = "Home";
+    private String type = "Dog";
+
+//    Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+//    Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+//    Constructor
+    public Pet(String name, int age, String location, String type) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.type = type;
+
+
+    }
+}
